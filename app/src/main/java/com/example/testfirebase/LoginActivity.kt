eff8f67e19f24e6a855d54a1aa2e176a45affd82
inf3417 <inf3417@hs-worms.de>
2020-04-25 13:19:22 +0200

    Second Commit

---
+++ app/src/main/java/com/example/testfirebase/LoginActivity.kt
@@ -0,0 +1,6 @@
+package com.example.testfirebase
+
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity: AppCompatActivity() {
+}
--- app/src/main/java/com/example/testfirebase/MainActivity.kt
+++ app/src/main/java/com/example/testfirebase/MainActivity.kt
@@ -2,11 +2,30 @@ package com.example.testfirebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var edt_username : EditText
+    private  lateinit var edt_email : EditText
+    private lateinit var edt_password : EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        edt_username = findViewById(R.id.editText_username)
+        edt_email = findViewById(R.id.editText_email)
+        edt_password = findViewById(R.id.editText_password)
+
+        button_register.setOnClickListener{
+            Log.d("MainActivity", "user "+edt_username.text.toString())
+            Log.d("MainActivity", "email: "+edt_email.text.toString())
+
+        }
+        textView_already_have_an_account.setOnClickListener {
+            Log.d("MainActivity", "ok!")
+        }
     }
 }
