c5028c9c3228aa49ee17e64b9e2c824f201c5059
inf3417 <inf3417@hs-worms.de>
2020-05-01 20:46:45 +0200

    I added Animations on "Start Activity"

--- app/src/main/java/com/example/testfirebase/StartActivity.kt
+++ app/src/main/java/com/example/testfirebase/StartActivity.kt
@@ -1,11 +1,30 @@
 package com.example.testfirebase
 
 import android.os.Bundle
+import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.start_activity.*
 
 class StartActivity: AppCompatActivity() {
+
+    private lateinit var topAnim : Animation
+    private lateinit var bottomAnim : Animation
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        // One way to hide the ActionBar(ColorPrimary and ColorPrimaryDark in styles.xml)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.start_activity)
+
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation)
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation)
+
+        //we assign each animation to his view
+        imageView_start_application_logo.startAnimation(topAnim)
+        textView_help_your_Neighbour.startAnimation(bottomAnim)
+        textView_we_are_in_solidarity.startAnimation(bottomAnim)
+
     }
 }
