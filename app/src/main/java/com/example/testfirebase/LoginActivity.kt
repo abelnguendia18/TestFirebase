bbaefcc544c4ed204ed6adf8c52ecb3a2623de25
inf3417 <inf3417@hs-worms.de>
2020-05-01 18:55:13 +0200

    I added "Start Activity"

--- app/src/main/java/com/example/testfirebase/LoginActivity.kt
+++ app/src/main/java/com/example/testfirebase/LoginActivity.kt
@@ -1,6 +1,19 @@
 package com.example.testfirebase
 
+import android.content.Intent
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
 
-class LoginActivity: AppCompatActivity() {
+class LoginActivity: AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        textView_return_create_account.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
 }
--- app/src/main/java/com/example/testfirebase/MainActivity.kt
+++ app/src/main/java/com/example/testfirebase/MainActivity.kt
@@ -1,18 +1,21 @@
 package com.example.testfirebase
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.EditText
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()
+{
 
     private lateinit var edt_username : EditText
     private  lateinit var edt_email : EditText
     private lateinit var edt_password : EditText
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         edt_username = findViewById(R.id.editText_username)
@@ -26,6 +29,8 @@ class MainActivity : AppCompatActivity() {
         }
         textView_already_have_an_account.setOnClickListener {
             Log.d("MainActivity", "ok!")
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
         }
     }
 }
